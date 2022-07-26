28a9a93051ebe0fc195fc7442a56928c2c75820c
Flavio Rodriguez <ihaio95@gmail.com>
2022-07-26 08:13:41 -0300

    deploy to vercel config

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-require("dotenv").config()
 const express = require("express")
+const cors = require('cors')
 const app = express()
-const PORT = process.env.APP_PORT || 3000;
 const routes = require("./routes")
+
+app.use(cors())
+
 require("./db")();
 
 app.use(express.json());
+
 routes(app)
 
-
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}!`))
+module.exports = app
